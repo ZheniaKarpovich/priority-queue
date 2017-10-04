41bbd85aaf863b3127355ca3b9671b33e8003272
ZheniaKarpovich <zhenia.karpovich@gmail.com>
2017-10-05 00:30:44 +0300

    add in MaxHeap constructor,clear,insertNode

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,7 +2,8 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root=null;
+		this.parentNodes=[];
 	}
 
 	push(data, priority) {
@@ -30,11 +31,46 @@ class MaxHeap {
 	}
 
 	clear() {
-		
+		this.root=null;
+		this.parentNodes=[];
 	}
 
 	insertNode(node) {
-		
+		if(this.root===null)
+		{
+			this.root=node;
+			this.parentNodes.push(node);
+		}
+		else{
+			var flag=false;
+			for(var i=0;i<this.parentNodes.length;i++)
+			{
+			//	console.log(this.parentNodes[i]);
+				if((this.parentNodes[i].left===null))
+				{
+					this.parentNodes[i].appendChild(node);
+					if((this.parentNodes[i].left!==null)&&(this.parentNodes[i].right!==null))
+					{
+						flag=true;
+					}
+					break;
+				}
+				else if((this.parentNodes[i].right===null))
+				{
+					this.parentNodes[i].appendChild(node);
+					if((this.parentNodes[i].left!==null)&&(this.parentNodes[i].right!==null))
+					{
+						flag=true;
+					}
+					break;
+				}
+			}
+			this.parentNodes.push(node);
+			if(flag===true)
+			{
+				this.parentNodes.shift();
+			}
+		}
 	}
 
 	shiftNodeUp(node) {
