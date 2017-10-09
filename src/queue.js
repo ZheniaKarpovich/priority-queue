1df3bcb51f5551055379e78d0b44349d2ca2b181
ZheniaKarpovich <zhenia.karpovich@gmail.com>
2017-10-10 00:50:12 +0300

    Queue

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,37 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(!arguments.length)
+			this.maxSize=30;
+		else
+			this.maxSize=maxSize;
+		this.heap=new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(this.size()<this.maxSize)
+		{
+			this.heap.push(data,priority);
+		}
+		else{
+			throw new Error('PriorityQueue has maxSize!!!');
+		}
 	}
 
 	shift() {
-
+		if(this.size()===0)
+			throw new Error('PriorityQueue is empty!!!')
+		else{
+			return this.heap.pop();
+		}
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
